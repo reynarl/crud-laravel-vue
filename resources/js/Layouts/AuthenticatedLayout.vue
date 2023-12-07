36b789ea45a1ac11d136a81fe3cf6995975b975b
Reeyna <reeynarl@gmail.com>
2023-12-07 15:03:36 -0600

    Update navbar in dashboard and add views for routes

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,6 +33,26 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </NavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('departaments.index')" :active="route().current('departaments.index')">
+                                    Departments
+                                </NavLink>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('employees.index')" :active="route().current('employees.index')">
+                                    Employees
+                                </NavLink>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('employees.graphic')" :active="route().current('employees.graphic')">
+                                    Graphics
+                                </NavLink>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('employees.reports')" :active="route().current('employees.reports')">
+                                    Reports
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -114,6 +134,18 @@ const showingNavigationDropdown = ref(false);
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('departaments.index')" :active="route().current('departaments.index')">
+                            Departments
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('employees.index')" :active="route().current('employees.index')">
+                            Employees
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('employees.graphic')" :active="route().current('employees.graphic')">
+                            Graphics
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('employees.reports')" :active="route().current('employees.reports')">
+                            Reports
                         </ResponsiveNavLink>
                     </div>
 
---
+++ resources/js/Pages/Departaments/Index.vue
@@ -0,0 +1,67 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import Swal from 'sweetalert2';
+
+const props = defineProps({
+    departaments: {type:Object}
+})
+
+const form = useForm({
+    id:''
+})
+
+const deleteDepartmentModal = (department) => {
+    const alert = Swal.mixin({
+        buttonsStyling:true
+    })
+    alert.fire({
+        title:'Estas seguro de eliminar este departamento?',
+        icon:'question', showCancelButton:true,
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if(result.isConfirmed){
+            form.delete(route('departaments.destroy', department))
+        }
+    })
+}
+</script>
+
+<template>
+    <Head title="Departments" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Departments</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="grid bg-white v-screen place-items-center">
+                <div class="bg-green-600 rounded-md px-2 py-2 text-white">
+                    <Link :href="route('departaments.create')">Add</Link>
+                </div>
+                <div>
+                <table class="table-auto border border-gray-400 mt-4">
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th>ID</th>
+                            <th>DEPARTMENT</th>
+                            <th colspan="2">OPTIONS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="departament, index in departaments" :key="departament.id">
+                            <td class="border border-gray-400 px-4 py-4">{{ index +1 }}</td>
+                            <td class="border border-gray-400 px-4 py-4">{{ departament.name }}</td>
+                            <td class="border border-gray-400 px-4 py-4"><Link :href="route('departaments.edit', departament)">Editar</Link></td>
+                            <td class="border border-gray-400 px-4 py-4"><button @click="deleteDepartmentModal(departament)">Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            </div>
+
+        </div>
+    </AuthenticatedLayout>
+
+</template>
