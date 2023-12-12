72b13ef121d4f61a6c1752be2e0bf9aac6978520
Reeyna <reeynarl@gmail.com>
2023-12-12 15:40:57 -0600

    Add line chart to show employees

--- resources/js/Pages/Employees/Graphic.vue
+++ resources/js/Pages/Employees/Graphic.vue
@@ -1,6 +1,34 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { Line } from 'vue-chartjs';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+
+  ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const props = defineProps({
+    data: {type:Object}
+})
+
+const data = {
+  labels: props?.data?.map(data => data.name),
+  datasets: [
+    {
+      label: 'Employees',
+      backgroundColor: '#f87975',
+      data: props.data?.map(data => data.count)
+    }
+  ]
+}
+
 </script>
 
 <template>
@@ -13,7 +41,9 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">x</div>
+                    <div class="p-6 text-gray-900">
+                        <Line :data="data"></Line>
+                    </div>
                 </div>
             </div>
         </div>
